b859ebb9e3deb5baf40781b51453c6a17cd8fb7b
Julio Fernandez <jfernandez74@gmail.com>
2019-09-22 19:28:35 +0200

    feature: added test framework

---
+++ tests/Unit/FaceMatchTraitTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Grananda\AwsFaceMatch\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class FaceMatchTraitTest extends TestCase
+{
+    /** @test */
+    public function event_is_triggered_on_model_save()
+    {
+        // Given
+
+        // When
+
+        // Then
+        $this->assertTrue(true);
+    }
+}
